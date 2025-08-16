c98258d267e962c0004e5bf02b4370bb2593ed50
Kavindu123444 <ffdkboy762@gmail.com>
2025-08-17 03:33:03 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~lvsgmCDa#9TaTx-KmH9Dd2kDMMGaiTEdrUa5zexyIA6BowTOkHVc",  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
